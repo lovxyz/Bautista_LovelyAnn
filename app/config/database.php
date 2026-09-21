51f05d8254d9181f616a88d78b6756eb441eb8ab
Lovely Ann Bautista <bautistalovelyann3@gmail.com>
2026-09-21 23:33:44 +0800

    Use environment variables for database

--- app/config/database.php
+++ app/config/database.php
@@ -58,14 +58,12 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-
-    'driver' => 'mysql',
-
-    'hostname' => 'mysql-1890ece7-bautistalovelyann3-c8f3.b.aivencloud.com',
-    'port'     => '11182',
-    'username' => 'avnadmin',
-    'password' => '',
-    'database' => 'mydb',
+    'driver'   => 'mysql',
+    'hostname' => getenv('DB_HOST') ?: '',
+    'port'     => getenv('DB_PORT') ?: '',
+    'username' => getenv('DB_USERNAME') ?: '',
+    'password' => getenv('DB_PASSWORD') ?: '',
+    'database' => getenv('DB_NAME') ?: '',
     'charset'  => 'utf8mb4',
     'dbprefix' => '',
     'path'     => ''
